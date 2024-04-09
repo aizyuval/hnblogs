ef31b5d851da948c4bcdb55a3ea7b75f60381d34
yuval <yuvalaizenman@gmail.com>
2024-04-09 20:49:12 +0300

    formatter. removed clutter

--- src/scraper/indexer/spiders/hnblogs_spider.py
+++ src/scraper/indexer/spiders/hnblogs_spider.py
@@ -1,56 +1,61 @@
 import scrapy
 from scrapy.spiders import Spider, CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor, IGNORED_EXTENSIONS # LinkExtractor is the same as LxmlLinkExtractor
+from scrapy.linkextractors import (
+    LinkExtractor,
+    IGNORED_EXTENSIONS,
+)   # LinkExtractor is the same as LxmlLinkExtractor
 from scrapy.exceptions import CloseSpider, IgnoreRequest
 from scrapy.http.response.text import TextResponse
-from scrapy.http import Request, HtmlResponse, XmlResponse
+from scrapy.http import Request, HtmlResponse
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 import logging
 from bs4 import BeautifulSoup
 import datetime
 from urllib.parse import urlsplit
-import feedparser
-from indexer.spiders.search_my_site_parser import customparser
+from indexer.spiders.hnblogs_parser import customparser
 import re
 
 
-IGNORED_EXTENSIONS += ['jar', 'json', 'cbr'] 
+IGNORED_EXTENSIONS += ['jar', 'json', 'cbr', 'xml']
+
 
 class HNblogsSpider(CrawlSpider):
-    name = "HNblogsSpider"
-"""
-Typesense Schema:
+    name = 'hnblogsspider'
+    """
+    Typesense Schema:
 
-blogs_schema = { 
-	    "name": "HNblogsSchema", 
-	    "fields": [ 
-	    { "name": "title", "type": "string" }, 
-	    { "name": "content", "type": "string" }, 
-	    { "name": "domain", "type": "string", "facet": true }, 
-	    { "name": "page-type", "type": "string"}, 
-	    { "name": "description", "type": "string", "optional": true }, 
-	    { "name": "anchor", "type": "string", "optional": true },  ##### remove
-	    { "name": "language", "type": "string", "optional": true }, 
-	    { "name": "url", "type": "string" }, 
-	    { "name": "content-type", "type": "string", "index": false, "optional": true}, 
-	    { "name": "page_last_modified", "type": "int64", "facet": true }, 
-	    { "name": "indexed_date", "type": "int64", "optional": true,"index": false, "optional": true }
- 
-	    ], 
- 
-	    "token_separators": ["_", "-"] 
-}
-"""
+    blogs_schema = { 
+                "name": "hnblogsschema", 
+                "fields": [ 
+                { "name": "title", "type": "string" }, 
+                { "name": "content", "type": "string" }, 
+                { "name": "domain", "type": "string", "facet": true }, 
+                { "name": "page-type", "type": "string"}, 
+                { "name": "description", "type": "string", "optional": true }, 
+                { "name": "anchor", "type": "string", "optional": true },  ##### remove
+                { "name": "language", "type": "string", "optional": true }, 
+                { "name": "url", "type": "string" }, 
+                { "name": "content-type", "type": "string", "index": false, "optional": true}, 
+                { "name": "page_last_modified", "type": "int64", "facet": true }, 
+                { "name": "indexed_date", "type": "int64", "optional": true,"index": false, "optional": true }
+     
+                ], 
+     
+                "token_separators": ["_", "-"] 
+    }
+    """
     custom_settings = {
         'ITEM_PIPELINES': {
-            'indexer.pipelines.TypesensePipeline': 300 # activate the item pipeline (that will add/commit parsed results)
+            'indexer.pipelines.TypesensePipeline': 300  # activate the item pipeline (that will add/commit parsed results)
         }
     }
 
     def __init__(self, *args, **kwargs):
         # Get kwargs
-        self.typesense_helper = kwargs.get('typesense_helper')
+        self.typesense_helper = kwargs.get(
+            'typesense_helper'
+        )   # to be used by the pipeline
         self.site_config = kwargs.get('site_config')
         self.home_page = self.site_config['home_page']
         self.domain = self.site_config['domain']
@@ -75,7 +80,7 @@ blogs_schema = {
             self.start_urls = [self.home_page, self.web_feed]
         else::"""
         self.start_urls = [self.home_page]
-        self.allowed_domains = [self.domain] 
+        self.allowed_domains = [self.domain]
 
         self.logger.info('Start URLs {}'.format(self.start_urls))
         self.logger.info('Allowed domains {}'.format(self.allowed_domains))
@@ -92,24 +97,33 @@ blogs_schema = {
         # Set up rule for configuring scraping behavior. Which links to extract, callback function, and following links
         self.rules = (
             Rule(
-                LinkExtractor(allow_domains=self.allowed_domains, deny=deny, deny_extensions=IGNORED_EXTENSIONS, tags=('a','area','link')),
-                callback=self.parse_item,
-                follow=True
+                LinkExtractor(
+                    allow_domains=self.allowed_domains,
+                    deny=deny,
+                    deny_extensions=IGNORED_EXTENSIONS,
+                    tags=('a', 'area', 'link'),
                 ),
-            )
-        super(HNblogsSpider, self).__init__(self, *args, **kwargs) # invoking the __init__ function of the CrawlSpider class, to not break crucial initialisations
+                callback=self.parse_item,
+                follow=True,
+            ),
+        )
+        super(HNblogsSpider, self).__init__(
+            self, *args, **kwargs
+        )   # invoking the __init__ function of the CrawlSpider class, to not break crucial initialisations
 
-    # As per https://docs.scrapy.org/en/latest/topics/spiders.html the "default implementation generates Request(url, dont_filter=True) 
+    # As per https://docs.scrapy.org/en/latest/topics/spiders.html the "default implementation generates Request(url, dont_filter=True)
     # for each url in start_urls"
-    # The dont_filter=True means that the start_urls bypass the deduplication, which means a 2nd home page could be indexed after the first, 
+    # The dont_filter=True means that the start_urls bypass the deduplication, which means a 2nd home page could be indexed after the first,
     # and the 2nd wouldn't have is_home=true, so we need to override this method
     def start_requests(self):
-        for url in self.start_urls:
+        for (
+            url
+        ) in self.start_urls:   # there's only one, so it does not matter.
             yield Request(url)
 
     # parse_start_url is called ONCE, for each start_urls. Here we override the function (from CrawlSpider) with our configuration.
     # it parses the web_feed for links, as they don't have theirs extracted by the LinkExtractor Rule.
-    # Because the LinkExtractor Rule only works where the response is an HtmlResponse 
+    # Because the LinkExtractor Rule only works where the response is an HtmlResponse
     def parse_start_url(self, response):
         configure_logging(get_project_settings())
         logger = logging.getLogger()
@@ -150,19 +164,22 @@ blogs_schema = {
             yield item
             # from here on LinkExtractor will get to work!
         else:
-            logger.warn('Skipping processing start_url: {}'.format(response.url))
+            logger.warn(
+                'Skipping processing start_url: {}'.format(response.url)
+            )
 
     def parse_item(self, response):
         configure_logging(get_project_settings())
         logger = logging.getLogger()
         logger.debug('Parsing URL: {}'.format(response.url))
-        #scrape_count = self.crawler.stats.get_value('item_scraped_count')
-        if not isinstance(response, TextResponse): #Text response is a class that is initiated when the response is html/xml
-            logger.info('Item {} is not a TextResponse, so skipping.'.format(response)) # Skip item if e.g. an image
+        # scrape_count = self.crawler.stats.get_value('item_scraped_count')
+        if not isinstance(
+            response, TextResponse
+        ):   # Text response is a class that is initiated when the response is html/xml
+            logger.info(
+                'Item {} is not a TextResponse, so skipping.'.format(response)
+            )   # Skip item if e.g. an image
         else:
             item = customparser(response, self.domain, self.site_config)
             if item:
-                yield item # go pipeline
-
-
-
+                yield item   # go pipeline
